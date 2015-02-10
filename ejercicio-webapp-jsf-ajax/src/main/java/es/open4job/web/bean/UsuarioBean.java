e92dd55ada4a5c255d72bfd030e799af19381d26
fsolans <francisco.solans@neodoo.es>
2015-02-10 21:40:58 +0100

    Ejemplo de Ajax en JSF 2

---
+++ ejercicio-webapp-jsf-ajax/src/main/java/es/open4job/web/bean/UsuarioBean.java
@@ -0,0 +1,31 @@
+package es.open4job.web.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class UsuarioBean {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String saludar() {
+	
+		if ( (nombre == null) || (nombre.equals("")) )
+			return "";
+		else
+			return "Hola " + nombre + " (Respuesta Ajax).";
+		
+	}
+	
+}
