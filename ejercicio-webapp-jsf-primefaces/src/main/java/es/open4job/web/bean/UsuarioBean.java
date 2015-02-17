a6610950eb4ebc004676a0f503ae071a303d13db
fsolans <francisco.solans@neodoo.es>
2015-02-17 07:39:13 +0100

    Ejemplo básico de PrimeFaces

---
+++ ejercicio-webapp-jsf-primefaces/src/main/java/es/open4job/web/bean/UsuarioBean.java
@@ -0,0 +1,32 @@
+package es.open4job.web.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class UsuarioBean {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String descripcion;
+
+	public String getDescripcion() {
+		return nombre;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
